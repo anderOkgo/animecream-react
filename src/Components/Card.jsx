172c9fcde8560ab45892eaa814a69a82d30279d1
anderson rangel <andergroundmsg@gmail.com>
2023-04-05 18:39:30 +1000

    add helper cyfer

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CardRow from './CardRow';
+import cyfer from '../helpers/cyfer.js';
 
 const Card = ({ data }) => {
+  //console.log(cyfer().dcy('PTE9Mj02PTU8-Dw3PTE8NA==', 'clave'));
   return (
     <div>
-      <h3>Results</h3>
+      <h3>{cyfer().cy('anderson', 'clave')}</h3>
       {data.data.length > 0 ? (
         data.data.map((el) => <CardRow key={el.production_ranking_number} el={el} />)
       ) : (
