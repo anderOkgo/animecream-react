a8ba3fd6386585d1d76e2867ddc3afc98884db4a
anderson rangel <andergroundmsg@gmail.com>
2023-06-28 01:16:17 +1000

    add command up

--- com/db-sync-FTP-both.bat
+++ com/db-sync-FTP-both.bat
@@ -1,11 +1,6 @@
 @ECHO OFF
 SETLOCAL
 IF EXIST setter.bat (CD.. & CALL com\setter.bat) ELSE ( CALL com\setter.bat )
-pause
-%LocalPath%
-pause
-%FTPFolder.txt%
-pause
 "keys/WinSCP.com" ^
   /command ^
     "open ftp://%FTPUsername.txt%:%FTPPss.txt%@%FTPserver.txt%" ^
