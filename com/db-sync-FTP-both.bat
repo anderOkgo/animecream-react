a4ba7124e3417146d8a379470eb87579e86722fc
anderson rangel <andergroundmsg@gmail.com>
2023-06-27 21:53:07 +1000

    add .txt a var

--- com/db-sync-FTP-both.bat
+++ com/db-sync-FTP-both.bat
@@ -1,11 +1,16 @@
 @ECHO OFF
 SETLOCAL
 IF EXIST setter.bat (CD.. & CALL com\setter.bat) ELSE ( CALL com\setter.bat )
+pause
+%LocalPath%
+pause
+%FTPFolder.txt%
+pause
 "keys/WinSCP.com" ^
   /command ^
     "open ftp://%FTPUsername.txt%:%FTPPss.txt%@%FTPserver.txt%" ^
     "option batch off" ^
-    "synchronize remote  ""%LocalPath%\"" /%FTPFolder.txt%" ^
+    "synchronize remote  ""%LocalPath.txt%\"" /%FTPFolder.txt%" ^
     "exit" ^
 IF %ERRORLEVEL% EQU 0 (Echo No error found) ELSE (Echo An error was found)
 PAUSE
