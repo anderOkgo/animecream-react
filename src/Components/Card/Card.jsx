b6bd7da943b17b94068590a1824a0466b6b553ff
anderson rangel <andergroundmsg@gmail.com>
2023-08-25 01:07:13 +1000

    add pagination method

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardRow from '../CardRow/CardRow';
 import cyfer from '../../helpers/cyfer.js';
 
 const Card = ({ data }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10; // You can adjust this according to your preference.
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
   return (
     <div>
       {data.length > 0 ? (
-        data.map((el) => <CardRow key={el.production_ranking_number} el={el} />)
+        currentItems.map((el) => <CardRow key={el.production_ranking_number} el={el} />)
       ) : (
         <div>No Data</div>
       )}
+
+      {/* Previous button */}
+      <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+        Previous
+      </button>
+
+      {/* Page numbers */}
+      {Array.from({ length: Math.ceil(data.length / itemsPerPage) }).map((_, index) => (
+        <button
+          key={index}
+          onClick={() => setCurrentPage(index + 1)}
+          className={currentPage === index + 1 ? 'active' : ''}
+        >
+          {index + 1}
+        </button>
+      ))}
+
+      {/* Next button */}
+      <button
+        onClick={() => setCurrentPage(currentPage + 1)}
+        disabled={currentPage === Math.ceil(data.length / itemsPerPage)}
+      >
+        Next
+      </button>
     </div>
   );
 };
--- src/Components/CardRow/CardRow.jsx
+++ src/Components/CardRow/CardRow.jsx
@@ -5,45 +5,6 @@ import PropTypes from 'prop-types';
 import './CardRow.css';
 
 export default function CardRow({ el }) {
-  function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-  };
-
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   let {
     production_name,
     production_year,
