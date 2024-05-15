e81386ce525686089bce92ded3248eb1cadb2d90
anderson rangel <andergroundmsg@gmail.com>
2024-05-14 19:44:14 -0500

    adjust hr hide to responsive

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Define the version of the cache
-const VERSION = '1.1.38';
+const VERSION = '1.1.39';
 // Create a unique cache name using the version
 const CACHE_NAME = `animecream-${VERSION}`;
 const appfiles = [`./android-icon-192x192.ico`];
