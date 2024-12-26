331a56a080625ecfe67126e386e61a70d24f8202
anderson3A <anderson@tresastronautas.com>
2024-12-26 16:36:04 -0500

    UI proportions fixed

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,21 @@
-// Define the version of the cache
-const VERSION = '1.1.60';
-// Create a unique cache name using the version
+const VERSION = '1.1.61';
 const CACHE_NAME = `animecream-${VERSION}`;
 const appfiles = [`./android-icon-192x192.ico`];
 
 self.addEventListener('install', (e) => {
-  // Wait until caching is complete
   e.waitUntil(
-    // Open the cache and add all app files to it
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(appfiles).then(() => {
-        // Log successful cache installation
         console.log('Cache installation successful');
-        // Activate the service worker immediately
         return self.skipWaiting();
       });
     })
   );
 });
 
-// Event listener for fetch requests
 self.addEventListener('fetch', (e) => {
-  // Respond to fetch request
   e.respondWith(
-    // Check if the requested resource is in the cache
     caches.match(e.request).then(async (res) => {
-      // If resource is found in cache or if res is false
       if (res || false)
         if (res.type !== 'cors' || !res.url.includes(self.location.origin))
           // If the resource is not a CORS request or doesn't belong to the same origin, return the cached response
@@ -33,32 +23,24 @@ self.addEventListener('fetch', (e) => {
       // If resource is not in cache or is not a CORS request or doesn't belong to the same origin, fetch it from the network
       return fetch(e.request)
         .then((response) => {
-          // Clone the response
           const responseClone = response.clone();
-          // Open the cache and store the response
           caches.open(CACHE_NAME).then((cache) => {
-            // If the request is a GET request, starts with 'http' and belongs to the same origin, cache the response
             if (e.request.method === 'GET' && e.request.url.startsWith('http') && e.request.mode !== 'cors') {
               cache.put(e.request, responseClone);
             }
           });
-          // Return the original response
           return response;
         })
         .catch(() => {
-          // If fetch fails, return a response from the cache
           return caches.match(e.request);
         });
     })
   );
 });
 
-// Event listener for when the service worker is activated
 self.addEventListener('activate', (e) => {
-  // List of caches to keep
   const cacheWhitelist = [CACHE_NAME];
 
-  // Wait until cache cleanup is complete
   e.waitUntil(
     caches
       .keys()
