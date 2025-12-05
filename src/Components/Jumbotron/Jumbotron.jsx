e2551fb8f364fc6d86d721e9786be7f3caa84226
anderson3as <anderson@tresastronautas.com>
2025-12-05 01:07:20 -0500

    fix: mark

--- src/Components/Jumbotron/Jumbotron.jsx
+++ src/Components/Jumbotron/Jumbotron.jsx
@@ -4,6 +4,7 @@ import './Jumbotron.css';
 const Jumbotron = ({ t }) => {
   return (
     <div className="jumbotron">
+      <h1 className="jumbotron-title">{t('welcome')}</h1>
       <p className="jumbotron-description">{t('jumdescription')}</p>
     </div>
   );
